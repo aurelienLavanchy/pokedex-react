903d4ca0210c98e38028effbe83fceb619eae3e2
Aurélien Lavanchy <lavanchy.aurelien@gmail.com>
2024-03-25 19:23:56 +0100

    React Basics 02 Challenge

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,12 @@
+function PokemonCard() {
+  return (
+    <article className="card">
+      <figure>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+      </figure>
+      <figcaption>Bulbasaur</figcaption>
+    </article>
+  );
+}
+
+export default PokemonCard;
