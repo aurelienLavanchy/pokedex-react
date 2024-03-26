ba95d555b2f32f1c58b2b642cb2c9232ffc6360b
Aurélien Lavanchy <lavanchy.aurelien@gmail.com>
2024-03-26 16:23:31 +0100

    React Basics 03

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import PokemonCard from "./components/PokemonCard";
-import "./App.css";
+import "./assets/styles/App.css";
 
 function App() {
   return (
     <div>
+      <blockquote>
+        Design copied from
+        <a href="https://bulbapedia.bulbagarden.net/wiki/Main_Page"> Bulbapedia</a>
+      </blockquote>
       <PokemonCard />
     </div>
   );
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,33 @@
+import styles from "../assets/styles/PokemonCard.module.css";
+
+// CSS classes
+const generalClasses = `${styles.grassType} ${styles.outsideBorder}`;
+const inner = `${styles.grassTypeInner}`;
+const contentText = `${styles.grassTypeContent} ${styles.name}`;
+const contentImg = `${styles.grassTypeContent} ${styles.illustration}`;
+// Array of (Pokemon) objects
+const PokemonList = [
+  {
+    name: "Bulbasaur",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "Mew",
+  },
+];
+
 function PokemonCard() {
+  const pokemon = { name: PokemonList[1].name, image: PokemonList[1].image };
+  let pokemonImage =
+    pokemon.image !== undefined ? <img className={contentImg} src={pokemon.image} /> : <p>???</p>;
+
   return (
-    <article className="card">
-      <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+    <article className={generalClasses}>
+      <figure className={inner}>
+        {pokemonImage}
+        <figcaption className={contentText}>{pokemon.name}</figcaption>
       </figure>
-      <figcaption>Bulbasaur</figcaption>
     </article>
   );
 }
