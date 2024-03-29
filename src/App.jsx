4916369abbc7ec4f14c430426f40537a9ecf224d
Aurélien Lavanchy <lavanchy.aurelien@gmail.com>
2024-03-29 10:23:11 +0100

    React Basics 08 - sharing state in a map

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 import PokemonCard from "./components/PokemonCard";
 import "./assets/styles/App.css";
-import pokemon from "./assets/data/pokemon.json";
+import pokemonList from "./assets/data/pokemon.json";
 import { useState } from "react";
 import NavBar from "./components/NavBar";
 
-/* {pokemon && pokemon.map((p) => <PokemonCard key={p.id} image={p.image} name={p.name} />)} */
-
 function App() {
   const [pokemonIndex, setPokemonIndex] = useState(0);
-  const handleNextClick = () => {
-    pokemonIndex < pokemon.length - 1 ? setPokemonIndex(pokemonIndex + 1) : null;
-  };
-  const handlePreviousClick = () => {
-    pokemonIndex > 0 ? setPokemonIndex(pokemonIndex - 1) : null;
-  };
 
   return (
     <div className="container">
@@ -21,13 +13,13 @@ function App() {
         Design of the cards copied from
         <a href="https://bulbapedia.bulbagarden.net/wiki/Main_Page"> Bulbapedia</a>
       </blockquote>
-      <NavBar handleNext={handleNextClick} handlePrevious={handlePreviousClick} />
+      <NavBar setPokemonIndex={setPokemonIndex} />
       <div className="cardList">
         {
           <PokemonCard
-            key={pokemon[pokemonIndex].id}
-            image={pokemon[pokemonIndex].image}
-            name={pokemon[pokemonIndex].name}
+            key={pokemonList[pokemonIndex].id}
+            image={pokemonList[pokemonIndex].image}
+            name={pokemonList[pokemonIndex].name}
           />
         }
       </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,31 @@
 import PropTypes from "prop-types";
 import "../assets/styles/NavBar.css";
+import pokemonArray from "../assets/data/pokemon.json";
 
-function NavBar({ handlePrevious, handleNext }) {
+function NavBar({ setPokemonIndex }) {
+  /* console.log(pokemonIndex);
+  console.log(setPokemonIndex); */
+  const handleClick = (index) => {
+    setPokemonIndex(index);
+  };
   return (
     <nav>
-      <button onClick={handlePrevious}>Previous</button>
-      <button onClick={handleNext}>Next</button>
+      <h1>Navigation</h1>
+      {pokemonArray &&
+        pokemonArray.map((p, index) => {
+          return (
+            <button key={p.id} onClick={() => handleClick(index)}>
+              {p.name}
+            </button>
+          );
+        })}
     </nav>
   );
 }
 
 NavBar.propTypes = {
-  handlePrevious: PropTypes.func.isRequired,
-  handleNext: PropTypes.func.isRequired,
+  /* pokemonIndex: PropTypes.number.isRequired, */
+  setPokemonIndex: PropTypes.func.isRequired,
 };
 
 export default NavBar;
